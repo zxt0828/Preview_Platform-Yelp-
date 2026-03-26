b5a7ab789a455dce2ddb4408746dd6ad4154bf70
zxt0828 <zhuangxiaotong27@gmail.com>
2026-03-26 15:50:55 -0700

    review platform

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -40,4 +41,42 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     log.debug("发送短信验证码成功，验证码：{}", code);
     return Result.ok();
   }
+
+  @Override
+  public Result login(LoginFormDTO loginForm, HttpSession session) {
+    //verify phone number
+    String phone = loginForm.getPhone();
+    if(RegexUtils.isPhoneInvalid(phone)) {
+      return Result.fail("wrong phone number");
+    }
+
+    //verify the verification code
+    String code = loginForm.getCode();
+    if(RegexUtils.isCodeInvalid(code)) {
+      return Result.fail("wrong code");
+    }
+
+    //find the user from database
+    User user = query().eq("phone", phone).one();
+
+    //if user do not exist
+    if(user == null){
+      user = createUserWithPhone(phone);
+    }
+
+    //store session
+    session.setAttribute("user", user);
+    return Result.ok();
+
+  }
+  // a method : if user not exist, create a new user with phone number
+  private User createUserWithPhone(String phone){
+    User user = new User();
+    user.setPhone(phone);
+    user.setNickName("user" + RandomUtil.randomNumbers(6));
+    save(user);
+    return user;
+  }
+
+
 }
